34af3d7b29204ef103edd366cd8f6c68967ac532
AleKolar <gefest-173@rambler.ru>
2024-10-08 23:50:18 +0300

    Implement submitData method

--- Sprint/settings.py
+++ Sprint/settings.py
@@ -78,25 +78,7 @@ WSGI_APPLICATION = 'Sprint.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql',
-#         'NAME': 'tbase',
-#         'USER': 'postgres',
-#         'PASSWORD': 'postgres',
-#         'HOST': 'localhost',
-#         'PORT': '5432',
-#         'OPTIONS': {
-#             'options': '-c client_encoding=UTF8 -c search_path=public'
-#         },
-#     }
-# }
+
 
 DATABASES = {
     'default': {
